fff5e2954d44534beab8d83b05844141f05b39a4
emiliomontes1 <emiliorenaudmontes@gmail.com>
2026-02-15 01:57:10 +0000

    Sovacoding lesson1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,5 @@
+print("Hello world!")
+print("My name is Emilio")
+print(("I dont like dogs"))
+print("I")
+#print(hi
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,11 @@
+x = 5 # integer: whole numbers without decimal
+print(x)
+
+
+#string: text enclosed in quotes 
+
+favorite_food = "pizza"
+print("x,favorite_food") # you can print multiple things on the same line using a comma
+
+y = 3.25 # float:number with a decimal
+print(y,"dogs",4)
